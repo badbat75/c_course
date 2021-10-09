6d968c6f71486d8d94f287d9a442d5e2ebfe06da
Emiliano De Simoni <emiliano.desimoni@outlook.com>
2021-10-09 13:32:54 +0200

    added cl parameters

--- BoxedString/BoxedString.c
+++ BoxedString/BoxedString.c
@@ -3,17 +3,49 @@
 #include <string.h>
 #include "BoxedString.h"
 
-int main() {
-    char hline[] = H_LINE;
-    char vline[] = V_LINE;
-    char inputstr[INPUT_MAX + 1];
+int main(int argc, char **argv) {
+    //char inputstr[INPUT_MAX + 1]; 
+    char inputstr[INPUT_MAX + 1]; 
+    char hline[INPUT_MAX + 1] = "o";
+    char vline[INPUT_MAX + 1] = "o";
+    if ( argc == 1)
+    {
+       printf ("\nInsert your string here: ");
+        #ifdef _MSC_VER
+            scanf_s ("%" INPUT_MAX_S "[^\n]", &inputstr, INPUT_MAX + 1);
+        #else
+            scanf ("%" INPUT_MAX_S "[^\n]", inputstr);
+        #endif
+    }
+    for (int i = 0; i < argc; ++i)
+    {
+        //printf("%s ", argv[i]);
+        switch (i) {
+        case 1:
+            #ifdef _MSC_VER
+                strcat_s (inputstr, sizeof(inputstr) -1, argv[1]);
+            #else
+                strncpy (inputstr, argv[1], sizeof(inputstr) -1);
+            #endif
+            break;
+        case 2:
+            #ifdef _MSC_VER
+                strcat_s (hline, sizeof(hline) -1, argv[2]);
+            #else
+                strncpy (hline, argv[2], sizeof(hline) - 1);
+            #endif
+            break;
+        case 3:
+            #ifdef _MSC_VER
+                strcat_s (vline, sizeof(vline) -1, argv[3]);
+            #else
+                strncpy (vline, argv[3], sizeof(vline) -1);
+            #endif
+            break;
+        }  
+    }
+    printf("\n");
 
-    printf ("\nInsert your string here: ");
-    #ifdef _MSC_VER
-        scanf_s ("%" INPUT_MAX_S "[^\n]", &inputstr, INPUT_MAX + 1);
-    #else
-        scanf ("%" INPUT_MAX_S "[^\n]", inputstr);
-    #endif
     plotString (inputstr, hline, vline);
 
     return 0;
